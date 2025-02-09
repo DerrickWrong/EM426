e8a417296aff0da3bca07328305550e05c29ffd4
Derrick Huang <Derrick Huang@host.docker.internal>
2025-02-09 09:20:21 -0500

    First Commit

---
+++ src/main/java/TestController.java
@@ -0,0 +1,21 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class TestController {
+
+	@FXML
+	private Label testLabel;
+
+	@FXML
+	protected void clickTestButton() {
+		
+		int x = 10;
+		int y = 6;
+		
+		int t = x + y;
+		
+		testLabel.setText("Welcome to JavaFX! X + Y = " + t);
+		
+	}
+	
+}
---
+++ src/main/java/utils/FxScheduler.java
@@ -0,0 +1,47 @@
+package utils;
+
+import javafx.application.Platform;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+/*
+ * Copyright 2017 Jacob Hassel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * This class aggregates all of the different flavors of JavaFX
+ * {@link Scheduler} available as part of this repository.
+ */
+public final class FxScheduler {
+
+	private static final Scheduler FX_THREAD = Schedulers.fromExecutor(Platform::runLater);
+
+	private FxScheduler() {
+		// No instance
+	}
+
+	/**
+	 * This is the standard implementation of a JavaFX scheduler. It is created via
+	 * {@link Schedulers#fromExecutor(Executor)}. This should be considered the
+	 * baseline for any performance testing done with comparing JavaFX schedulers.
+	 *
+	 * @return A {@link Scheduler} that provides access to the JavaFX Application
+	 *         Thread.
+	 */
+	public static Scheduler fxThread() {
+		return FX_THREAD;
+	}
+
+}
